0eca24961a55556a987ba91b07cb81b5020b8a4c
haassanjavedawan <hassanjaved836@gmail.com>
2022-03-10 17:47:52 +0500

    cahnge nav

--- src/components/element/PageNotFound.js
+++ src/components/element/PageNotFound.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const PageNotFound = () => {
   return (
-    <div id="wrapper " className="notfound m-auto">
-      <img src="https://i.imgur.com/qIufhof.png" />
+    <div id="wrapper " className="notfound m-auto pt-5 m5-5">
+      <img className="w-100" src="https://i.imgur.com/qIufhof.png" />
       <div id="info">
         <h3>This page could not be found</h3>
       </div>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,118 +1,129 @@
 import { useState, useEffect } from 'react'
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import { Link as Links } from "react-scroll";
+
+
+import { Link  } from "react-scroll";
 
 export default function Menu() {
-    const [show, setShow] = useState(false);
-    const [scrolled, setScrolled] = useState(false);
-
-    const showDropdown = (e) => {
-        setShow(!show);
-    }
-    const hideDropdown = e => {
-        setShow(false);
-    }
-    const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 600) {
-            setScrolled(true);
-        }
-        else {
-            setScrolled(false);
-        }
-    }
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-    })
+  
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark-v" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src="/assets/logo-hr.png"
-                        width="160"
-                        height="33"
-                        className="d-inline-block align-top"
-                    />{' '}
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
+        <nav
+          className="navbar navbar-expand-lg navbar-light navbar-warpper"
+      
+        >
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              <img src="/assets/.png" width="150" alt="" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarText"
+              aria-controls="navbarText"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+              <ul className="navbar-nav me-auto m-auto  mb-lg-0">
+                <li className="nav-item h_nav-item">
+                  <Link
+                    activeClass="active"
+                    to="hero"
+                    delay={100}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                    className="nav-link h_nav-link"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item h_nav-item">
+                  <a
+                    href="/ "
+                    target="_blank"
+                    className="nav-link h_nav-link"
+                  >
+                    about
+                  </a>
+                </li>
 
-                    </Nav>
-                    <Nav>
-                   
-                        <Nav.Link href="/whitepaper.pdf" target='_blank'>
-                            Whitepaper
-                        </Nav.Link>
-                        <Nav.Link href="/" target='_blank'>
-                            Stake
-                        </Nav.Link>
-                        <Nav.Link href="/governance" target='_blank'>
-                            Vote
-                        </Nav.Link>
-                        <Nav.Link href="/" target='_blank'>
-                            Create Token
-                        </Nav.Link>
-                        <NavDropdown
-                            title="More"
-                            id="collasible-nav-dropdown"
-                            show={show}
-                            onMouseEnter={showDropdown}
-                            onMouseLeave={hideDropdown}
-                        >
-                            <Links
-                                to="team"
-                                spy={false}
-                                smooth={false}
-                                offset={-70}
-                                duration={500}
-                            >
-                                <NavDropdown.Item onClick={() => setShow(false)}>
-                                    Team
-                                </NavDropdown.Item>
-                            </Links>
-                            <Links
-                                to="timeline"
-                                spy={false}
-                                smooth={false}
-                                offset={-70}
-                                duration={500}
-                            >
-                                <NavDropdown.Item onClick={() => setShow(false)}>
-                                    Roadmap
-                                </NavDropdown.Item>
-                            </Links>
-                            <Links
-                                to="tokonomics"
-                                spy={false}
-                                smooth={false}
-                                offset={-70}
-                                duration={500}
-                            >
-                                <NavDropdown.Item onClick={() => setShow(false)}>
-                                    Tokonomics
-                                </NavDropdown.Item>
-                            </Links>
-                            <Links
-                                to="faqs"
-                                spy={false}
-                                smooth={false}
-                                offset={-70}
-                                duration={500}
-                            >
-                                <NavDropdown.Item onClick={() => setShow(false)}>
-                                    FAQs
-                                </NavDropdown.Item>
-                            </Links>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                <li className="nav-item h_nav-item">
+                  <Link
+                    activeClass="active"
+                    to="feature"
+                    delay={100}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                    className="nav-link h_nav-link"
+                  >
+                    Feature
+                  </Link>
+                </li>
+                <li className="nav-item h_nav-item">
+                  <Link
+                    activeClass="active"
+                    to="roadmap"
+                    delay={100}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                    className="nav-link h_nav-link"
+                  >
+                    Road Map
+                  </Link>
+                </li>
+                <li className="nav-item h_nav-item">
+                  <Link
+                    activeClass="active"
+                    to="tokenomics"
+                    delay={100}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                    className="nav-link h_nav-link"
+                  >
+                    Feature
+                  </Link>
+                </li>
+
+                <li className="nav-item h_nav-item">
+                  <Link
+                    activeClass="active"
+                    to="faqs"
+                    delay={100}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                    className="nav-link h_nav-link"
+                  >
+                    FAQ
+                  </Link>
+                </li>
+                {/* <li className="nav-item h_nav-item">
+                  <a className="nav-link h_nav-link ">
+                    <img src="assets/playstore.svg"  alt="" />
+                  </a>
+                </li>
+                <li className="nav-item h_nav-item ">
+                  <a className="nav-link h_nav-link ">
+                    <img src="assets/applestore.svg" alt="" />
+                  </a>
+                </li> */}
+              </ul>
+              <span className="navbar-text">
+                <a className="nav-btn  m-auto " target='_blank' href="">Play Game</a>
+              </span>
+            </div>
+          </div>
+        </nav>
     )
 }
